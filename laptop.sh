19cba7cc2b68494e4e463973dab0f3b5b3d8ccc4
Jon Fuller <jcfuller@sep.com>
2017-01-04 17:23:26 -0500

    source rvm so installing ruby works

--- laptop.sh
+++ laptop.sh
@@ -49,6 +49,7 @@ heroku login
 # ruby + rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
 
 rvm install 2.4
 gem update --system
